735b55f81b2928252e617e45549449d1eea75b22
Qoiuy <1010255393@qq.com>
2015-08-20 16:44:23 +0800

    用c3p0实现事务回滚操作。嘿嘿

--- Java_Demo/simple_transaction_action/src/demo/Dbutils.java
+++ Java_Demo/simple_transaction_action/src/demo/Dbutils.java
@@ -1,5 +1,7 @@
-package utils;
+package demo;
 
+import java.sql.Connection;
+import java.sql.Savepoint;
 import java.util.List;
 
 import org.apache.commons.dbutils.QueryRunner;
@@ -13,13 +15,39 @@ import bean.Account;
 
 
 public class Dbutils {
-	private QueryRunner runner = new QueryRunner(new ComboPooledDataSource()); 
+	ComboPooledDataSource data = new ComboPooledDataSource();
+	//Connection conn = data.getConnection();
+	private QueryRunner runner = new QueryRunner(data); 
+	/**
+	 * 用c3p0使用事务回滚到某个时间点
+	 * @author Lilac
+	 * 
+	 */
+	public void demo () throws Exception{
+		Connection conn = data.getConnection();
+		conn.setAutoCommit(false);
+		
+		int rows = runner.update("update account set sal=? where name=?", 100,"xiaoyi");
+		Savepoint sp = conn.setSavepoint();
+		int rows1 = runner.update("update account set sal=? where name=?", 500,"xiaor");
+		conn.rollback(sp);
+		List<Account> account = runner.query("select * from account", new BeanListHandler<Account>(Account.class));
+		for (Account account2 : account) {
+			System.out.println(account2.getId()+" ::"+account2.getName()+" ::"+ account2.getSal());
+		}
+	}
+	@Test
+	public void test ()throws Exception{
+		demo();
+	}
+	
 	/**
 	 * 向数据库中添加一条记录
 	 * @author Lilac
 	 * @throws Exception
 	 */
 	public void add_account() throws Exception {
+		
 		int rows = runner.update("insert into account values(?,?,?)", "101","lisi",500);
 		System.out.println(rows);
 	}	
